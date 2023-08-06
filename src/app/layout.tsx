328eb55cf14ba8112c2f9b0f6b1a059f5c655cd5
Laurens <lauverhaar@gmail.com>
2023-08-06 15:46:04 +0200

    feat: :chart_with_upwards_trend: Add Google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import { Content } from "../components/Content";
 import { Footlet } from "../components/Footlet";
 import { NavBar } from "../components/NavBar";
@@ -22,6 +23,16 @@ export default function RootLayout({
 				<link rel="shortcut icon" href="/img/favicon.png" type="image/x-icon" />
 			</head>
 			<body className="bg-zinc-950 text-white h-screen md:overflow-hidden">
+				<Script
+					strategy="afterInteractive"
+					src="https://www.googletagmanager.com/gtag/js?id=G-M58C29JPP3"
+				/>
+				<Script
+					strategy="afterInteractive"
+					dangerouslySetInnerHTML={{
+						__html: `window.dataLayer = window.dataLayer||[];function gtag(){dataLayer.push(arguments);};gtag('js',new Date());gtag('config','G-M58C29JPP3',{page_path:window.location.pathname});`,
+					}}
+				/>
 				<NavBar></NavBar>
 				<Content>{children}</Content>
 				<Footlet></Footlet>
